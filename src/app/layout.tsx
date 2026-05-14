be8ca46d9a7ef37655181dd842ad5fa7e49170d7
nivekdev99 <nivek9922@gmail.com>
2026-05-14 08:32:26 -0500

    feat speed-Insights: Se implementa esta herramienta de vercel gratuita apra el monitoreo tecnico en tiempo real de usuarios en mi pagina.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { notoSerif, manrope } from "@/config/fonts";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 import { Providers } from "@/components";
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body className="font-sans">
         <Providers>{children}</Providers>
         {GA_ID && <GoogleAnalytics gaId={GA_ID} />}
+        <SpeedInsights />
       </body>
     </html>
   );
